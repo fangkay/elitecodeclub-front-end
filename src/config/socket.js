e643f6a43b23b6a730ee3627ab5c919f2c1f7a28
fangkay <fangkay28@gmail.com>
2022-07-22 14:39:32 +0200

    Updated cors with headers

--- src/config/socket.js
+++ src/config/socket.js
@@ -1,5 +1,10 @@
 import io from "socket.io-client";
-const socket = io.connect("https://bidbybid.herokuapp.com");
+const socket = io.connect("https://bidbybid.herokuapp.com", {
+  withCredentials: true,
+  extraHeaders: {
+    "my-custom-header": "abcd",
+  },
+});
 
 socket.on("some_message", (data) => {
   console.log("I just got a message through the socket", data);
